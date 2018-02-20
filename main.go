4b5086b99ea8c0572d3a923341c1d16bf1322d96
wncbb <todd@toddmac.local>
2018-02-20 16:22:58 +0800

    1 commit

---
+++ main.go
@@ -0,0 +1,111 @@
+package my1Project
+
+import (
+	"github.com/gin-gonic/gin"
+	//"fmt"
+	"sort"
+	"io"
+	"strings"
+	"crypto/sha1"
+	"fmt"
+	"github.com/akkuman/parseConfig"
+)
+
+func getArgTest(c *gin.Context) {
+	name := c.DefaultQuery("name", "default")
+	c.JSON(200, gin.H{
+		"name":   name,
+		"method": "GET",
+	})
+}
+
+func makeSignature(timestamp, nonce, echostr string) string{
+	strSlice := []string{timestamp, nonce, token}
+	sort.Sort(sort.StringSlice(strSlice))
+	srcStr:=strings.Join(strSlice, "")
+	fmt.Println(srcStr)
+	sha1obj:=sha1.New()
+	io.WriteString(sha1obj, srcStr)
+	sha1rst:=sha1obj.Sum(nil)
+	return fmt.Sprintf("%x", sha1rst)
+}
+
+func getSignature(c *gin.Context){
+	signature:=c.DefaultQuery("signature", "")
+	timestamp:=c.DefaultQuery("timestamp", "")
+	nonce:=c.DefaultQuery("nonce", "")
+	echostr:=c.DefaultQuery("echostr", "")
+	rightSignature:=makeSignature(timestamp, nonce, echostr)
+
+	data:=make(map[string]string)
+	data["in_signature"]=signature
+	data["in_timestamp"]=timestamp
+	data["in_nonce"]=nonce
+	data["echostr"]=echostr
+
+	data["out_signature"]=rightSignature
+
+	if(signature==rightSignature){
+		c.JSON(200, gin.H{
+			"code": "0",
+			"msg": "success",
+			"data": data,
+		})
+	}else{
+		c.JSON(200, gin.H{
+			"code": "-1",
+			"msg": "error",
+			"data": data,
+		})
+	}
+
+}
+
+func postArgTest(c *gin.Context){
+	name:=c.DefaultPostForm("name", "default")
+	c.JSON(200, gin.H{
+		"name": name,
+		"method": "POST",
+	})
+}
+
+func getIP(c *gin.Context){
+	ip:=c.ClientIP()
+	c.JSON(200, gin.H{
+		"ip": ip,
+	})
+}
+
+func getHeader(c *gin.Context){
+	c.JSON(200, gin.H{
+		"header": c.Request.Header,
+	})
+}
+
+func abort(c *gin.Context){
+	c.AbortWithStatusJSON(403, "test")
+}
+
+var token string
+
+func Run(configFile string){
+	var config = parseConfig.New(configFile)
+	// 此为interface{}格式数据
+	token = (config.Get("token")).(string)
+
+	r:=gin.Default()
+	r.GET("/", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"status": "OK",
+			"code": 0,
+		})
+	})
+
+	r.GET("/getArg", getArgTest)
+	r.GET("/ip", getIP)
+	r.GET("/header", getHeader)
+	r.GET("/abort", abort)
+	r.GET("/signature", getSignature)
+
+	r.Run(":8003")
+}
